c528ad7fb6acc47cdb4e8a016b80453714829a5a
vuei91 <wkwk2805@gmail.com>
2025-09-14 13:28:14 +0900

    next, prev 버튼 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import HTMLFlipBook from "react-pageflip";
 import DnD from "../../components/DnD";
 import Page from "../../components/Page";
 import { useModeState } from "../../store";
+import { useEffect, useRef } from "react";
 
 export default function MyBook() {
   const { mode, toggleMode } = useModeState();
@@ -35,10 +36,47 @@ export default function MyBook() {
 
 const Container = (props: any) => {
   const { mode } = useModeState();
+  const ref = useRef(null);
+
+  const onClick = () => {
+    if (ref.current) {
+      // @ts-ignore
+      ref.current.pageFlip().flipNext();
+    }
+  };
+  const onClickPrev = () => {
+    if (ref.current) {
+      // @ts-ignore
+      ref.current.pageFlip().flipPrev();
+    }
+  };
 
   return mode === "VIEW" ? (
-    <HTMLFlipBook {...props} />
+    <>
+      <HTMLFlipBook {...props} ref={ref} />
+      <br />
+      <br />
+      <button
+        style={{ margin: 20, border: "1px solid", background: "red" }}
+        onClick={onClick}
+      >
+        next
+      </button>
+      <button
+        style={{ margin: 20, border: "1px solid", background: "red" }}
+        onClick={onClickPrev}
+      >
+        prev
+      </button>
+    </>
   ) : (
-    <HTMLFlipBook useMouseEvents={false} {...props} />
+    <>
+      <HTMLFlipBook useMouseEvents={false} {...props} />
+      <br />
+      <br />
+      <button style={{ margin: 20 }} onClick={onClick}>
+        d
+      </button>
+    </>
   );
 };
